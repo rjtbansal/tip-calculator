af4016eeab5422d48ee7a7e4d89a98e27090ac19
Rajat Bansal <rjtbansal@gmail.com>
2021-08-07 13:06:38 -0400

    using immer to mutate state (variation 1)

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -5,6 +5,7 @@ import {
   ITEM_REMOVED
 } from './actions';
 
+import { produce } from 'immer';
 let id = 1;
 
 export const initialItems = [
@@ -13,27 +14,56 @@ export const initialItems = [
 ];
 
 export const reducer = (state = initialItems, action) => {
+  //before immer library
+  // if (action.type === ITEM_ADDED) {
+  //   const item = { uuid: id++, quantity: 1, ...action.payload };
+  //   return [...state, item];
+  // }
+
   if (action.type === ITEM_ADDED) {
-    const item = { uuid: id++, quantity: 1, ...action.payload };
-    return [...state, item];
+    /**immer libraries produce function takes in the state and a function with an argument using which we can mutate the state directly
+     * immer library takes care of taking care of creating copies for us so we dont have to do tedious complex object/array copying manually
+     */
+    return produce(state, (draftState) => {
+      const item = { uuid: id++, quantity: 1, ...action.payload };
+      draftState.push(item);
+    });
   }
+
   if (action.type === ITEM_REMOVED) {
     return state.filter((item) => item.uuid !== action.payload.item_id);
   }
   if (action.type === ITEM_PRICE_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.item_id) {
-        return { ...item, price: action.payload.price };
-      }
-      return item;
+    //before immer
+    // return state.map((item) => {
+    //   if (item.uuid === action.payload.item_id) {
+    //     return { ...item, price: action.payload.price };
+    //   }
+    //   return item;
+    // });
+
+    //with immer
+    return produce(state, (draftState) => {
+      const item = draftState.find(
+        (item) => item.uuid === action.payload.item_id
+      );
+      item.price = action.payload.price;
     });
   }
+
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.item_id) {
-        return { ...item, quantity: action.payload.quantity };
-      }
-      return item;
+    // return state.map((item) => {
+    //   if (item.uuid === action.payload.item_id) {
+    //     return { ...item, quantity: action.payload.quantity };
+    //   }
+    //   return item;
+    // });
+
+    return produce(state, (draftState) => {
+      const item = draftState.find(
+        (item) => item.uuid === action.payload.item_id
+      );
+      item.quantity = action.payload.quantity;
     });
   }
   return state;
